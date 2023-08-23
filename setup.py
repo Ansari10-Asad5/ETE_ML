0722160373ad654552d381cff98019c62683b92d
Asad <asad8600571576@gmail.com>
2023-08-23 11:15:11 +0530

    setup

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import find_packages,setup
+from typing import List
+
+def get_requirements(file_path:str)->List[str]:
+    '''
+    This function will return the lsit of requirements
+    '''
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        [req.replace('\n','') for req in requirements]
+        if '-e .' in requirements:
+            requirements.remove('-e .')
+    return requirements
+print(get_requirements('requirements.txt'))
+setup(
+    name='MLProject',
+    version='0.0.1',
+    author='Asad',
+    author_email='asad8600571576@gmail.com',
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt')
+
+
+)
